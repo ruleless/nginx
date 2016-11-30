f54d0b8e92d5e80e6345b96996d8c6e41b0425fe
ruleless <ruleless@126.com>
2016-11-30 19:54:09 +0800

    add core module

--- src/port_map/ngx_pmap.c
+++ src/port_map/ngx_pmap.c
@@ -3,6 +3,8 @@
 #include "ngx_pmap.h"
 
 static char *ngx_pmap_block(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static void *ngx_pmap_core_create_conf(ngx_cycle_t *cycle);
+static void *ngx_pmap_core_init_conf(ngx_cycle_t *cycle, void *conf);
 
 static ngx_command_t ngx_pmap_commands[] = {
 	{
@@ -38,8 +40,55 @@ ngx_module_t ngx_pmap_module = {
 	NGX_MODULE_V1_PADDING
 };
 
+static ngx_str_t pmap_core_name = ngx_string("port_map");
+
+static ngx_command_t ngx_pmap_core_commands[] = {
+	{
+		ngx_string("endpoint"),
+		NGX_PMAP_CONF|NGX_CONF_TAKE1,
+		NULL,
+		0,
+		0,
+		NULL
+	},
+
+	ngx_null_command
+};
+
+ngx_pmap_module_t ngx_pmap_core_module_ctx = {
+	&pmap_core_name,
+
+	ngx_pmap_core_create_conf,
+	ngx_pmap_core_init_conf,
+};
+
+ngx_module_t ngx_pmap_core_module = {
+	NGX_MODULE_V1,
+	&ngx_pmap_core_module_ctx,
+	ngx_pmap_core_commands,
+	NGX_PMAP_MODULE,
+	NULL,                                  /* init master */
+    NULL,                 				   /* init module */
+    NULL,					               /* init process */
+    NULL,                                  /* init thread */
+    NULL,                                  /* exit thread */
+    NULL,                                  /* exit process */
+    NULL,                                  /* exit master */
+    NGX_MODULE_V1_PADDING
+};
+
 static char *
 ngx_pmap_block(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
 	
+}
+
+static void *
+ngx_pmap_core_create_conf(ngx_cycle_t *cycle)
+{	
+}
+
+static void *
+ngx_pmap_core_init_conf(ngx_cycle_t *cycle, void *conf)
+{
 }
--- src/port_map/ngx_pmap.h
+++ src/port_map/ngx_pmap.h
@@ -4,10 +4,39 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-#define NGX_PMAP_CONF	0x02000000
+#define NGX_PMAP_MODULE				0x50414D50
+#define NGX_PMAP_CONF				0x02000000
+
+#define NGX_PMAP_ENDPOINT_CLIENT	1
+#define NGX_PMAP_ENDPOINT_SERVER	2
 
 typedef struct {
-	
+    union {
+        struct sockaddr sockaddr;
+        struct sockaddr_in  sockaddr_in;
+#if (NGX_HAVE_INET6)
+        struct sockaddr_in6 sockaddr_in6;
+#endif
+#if (NGX_HAVE_UNIX_DOMAIN)
+        struct sockaddr_un sockaddr_un;
+#endif
+        u_char sockaddr_data[NGX_SOCKADDRLEN];
+    } u;
+
+    socklen_t socklen;
+    int backlog;
+} ngx_pmap_listen_t;
+
+typedef struct {
+	int endpoint;
+	ngx_pmap_listen_t listen;
 } ngx_pmap_conf_t;
+
+typedef struct {
+	ngx_str_t *name;
+
+	void *(*create_conf)(ngx_cycle_t *cycle);
+	void *(*init_conf)(ngx_cycle_t *cycle, void *conf);
+} ngx_pmap_module_t;
 
 #endif /* _NGX_PMAP_H_INCLUDED_ */
